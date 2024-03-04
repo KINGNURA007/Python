6acd279bcd2034299d067d1157c03449f9958165
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-04 22:18:23 +0530

    Update PythonNotes.py

--- PythonNotes.py
+++ PythonNotes.py
@@ -1,3 +1,4 @@
+#Python Intro
 """What is Python?
 Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
 Python is a general Purpose high-level interpreted language with easy syntax and dynamic semantics.
@@ -42,5 +43,6 @@ Python Syntax compared to other programming languages!
 Python was designed for readability, and has some similarities to the English language with influence from mathematics.
 Python uses new lines to complete a command, as opposed to other programming languages which often use semicolons or parentheses.
 Python relies on indentation, using whitespace, to define scope; such as the scope of loops, functions and classes. Other programming languages often use curly-brackets for this purpose."""
-#Example
+
+#Python Syntax
 print ('Hello World!')
