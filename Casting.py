fc4b0293522fb69dc764d021ab774bfa22d66e13
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 13:57:32 +0530

    Update Casting.py

--- Casting.py
+++ Casting.py
@@ -1,8 +1,10 @@
 """
 What is Casting?
       If you want to specify the data type of a variable, this can be done with casting.
-
-Example
+Example"""
 x = str(Casting Notes)  # x will be 'Casting Notes'
 y = int(3)              # y will be 3
 z = float(3)            # z will be 3.0
+print (x)
+print (y)
+print (z)
