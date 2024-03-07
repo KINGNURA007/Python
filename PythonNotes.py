057305eb85043dfbe8c7c64342635fe218717654
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 11:47:01 +0530

    Update PythonNotes.py

--- PythonNotes.py
+++ PythonNotes.py
@@ -88,6 +88,12 @@ if 5 > 2:
 if 5 > 2:
     print ("Five is greater than two")
 
+You have to use the same number of spaces in the same block of code, otherwise Python will give you an error:
+Example
+if 5 > 2:
+ print ("Five is greater than two")
+    print ("Five is greater than two")
+
 """
 
 for i in range (5):
