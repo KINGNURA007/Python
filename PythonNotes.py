f19a7daaf5914fed14149c188293fc261f07977f
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 12:22:45 +0530

    Update PythonNotes.py

--- PythonNotes.py
+++ PythonNotes.py
@@ -51,7 +51,7 @@ Python Syntax
 
 what is Python Syntax?
         Syntax is the arrangement of words and phrases to create well-formed sentences in a language.
-Python Syntax:
+Syntax Types:
         Execute Python Syntax
         Python Indentation
         Python Variables
@@ -97,10 +97,20 @@ if 5 > 2:
 
 What is Python Variables?
         Variables is a reserved memory location to store values.
+Variable Types:
+        n..,-2,-1,0,-1,2,..n    is a Integer
+        a-z or A-Z              is a String
+        Container name = Variable Name
+        Storage container = Variable
+        Underscore Character is a Variable
 Example
 Variables in Python:
-x = 5
-y = "Hello, World!"
+x = 5                 # x is a Variable name        5 is a Variable
+y = "Hello, World!"   # y is a Variable name        "Hello, World!" is a Variable
+print (x)
+print (y)
+
+
 
 """
 
