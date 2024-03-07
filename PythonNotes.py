917633cf77d24821647013aa1e16b72139227351
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 14:23:32 +0530

    Update PythonNotes.py

--- PythonNotes.py
+++ PythonNotes.py
@@ -148,6 +148,16 @@ y = 'John'
 print (x)
 print (y)
 
+Case-Sensitive
+        Variable names are case-sensitive.
+Example
+This will create two variables:
+a = 4
+A = "Sally"
+#A will not overwrite a
+print (a)
+print (A)
+
 Python Comments
 ---------------
 
