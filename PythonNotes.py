149837e7ddf7d1badf24c174bf46889634902e0b
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 11:31:45 +0530

    Update PythonNotes.py

--- PythonNotes.py
+++ PythonNotes.py
@@ -59,20 +59,32 @@ Python Syntax:
 Execute Python Syntax
 ---------------------
         Execute Python syntax can be executed by writing directly in the Command Line:
+
+Example
 >>> print ('Hello, World!')
 Hello, World!
 
 Or by creating a python file on the server, using the .py file extension, and running it in the Command Line:
+
+Example
 arun@linux:~/path/to/code/location$ python3 PythonNotes.py
 Hello, World!
 
 Python Indentation
 ------------------
         Indentation refers to the spaces at the beginning of a code line.
-
 Where in other programming languages the indentation in code is for readability only, the indentation in Python is very important.
-
 Python uses indentation to indicate a block of code.
+
+Example
+if 5 > 2:
+    print ("Five is greater than two")
+
+Example
+Syntax Error:
+if 5 > 2:
+print ("Five is greater than two")
+
 """
 
 for i in range (5):
