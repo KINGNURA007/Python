5838b57706c75a7b396f0aa220ea6f94976ac698
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 12:59:10 +0530

    Update Comments.py

--- Comments.py
+++ Comments.py
@@ -26,9 +26,7 @@ print("Cheers, Mate!")
 Multiline Comments
         Python does not really have a syntax for multiline comments.
 Python will ignore string literals that are not assigned to a variable, you can add a multiline string (triple quotes) in your code, and place your comment inside it:
-Example
-This is a comment written in more than just one line
+Example"""
+"""This is a comment written in more than just one line"""
 print("Hello, World!")
-
-
 """
