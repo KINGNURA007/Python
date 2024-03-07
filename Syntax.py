ff4eca822c51dc9abd4f9263f3761933f9afa932
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 12:02:49 +0530

    Update Syntax.py

--- Syntax.py
+++ Syntax.py
@@ -1,5 +1,9 @@
-#Syntax is the arrangement of words and phrases to create well-formed sentences in a language.
-
-for i in range (5):
- print (i)
+"""
+Python Syntax
+        Syntax is the arrangement of words and phrases to create well-formed sentences in a language.
+Python Syntax:
+        Execute Python Syntax
+        Python Indentation
+        Python Variables
+        Python Comments
 
