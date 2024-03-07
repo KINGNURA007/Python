dc46dffa4e290969924039018df048da5f589d4b
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 11:45:12 +0530

    Update PythonNotes.py

--- PythonNotes.py
+++ PythonNotes.py
@@ -73,12 +73,20 @@ Where in other programming languages the indentation in code is for readability 
 Python uses indentation to indicate a block of code.
 Example
 if 5 > 2:
-    print ("Five is greater than two")
+ print ("Five is greater than two")
 
+Python will give you an error if you skip the indentation:
 Example
 Syntax Error:
 if 5 > 2:
 print ("Five is greater than two")
+
+The number of spaces is up to you as a programmer, the most common use is four, but it has to be at least one.
+Example
+if 5 > 2:
+ print ("Five is greater than two")
+if 5 > 2:
+    print ("Five is greater than two")
 
 """
 
