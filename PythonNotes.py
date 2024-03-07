95a424516be277f5fab7c66c90f37a9d12ea2e0b
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 13:41:58 +0530

    Update PythonNotes.py

--- PythonNotes.py
+++ PythonNotes.py
@@ -96,7 +96,10 @@ if 5 > 2:
     print ("Five is greater than two")
 
 What is Python Variables?
-        Variables is a reserved memory location to store values.
+        Variables are containers for storing data values or Variables is a reserved memory location to store values.
+Creating Variables:
+        Python has no command for declaring a variable.
+A variable is created the moment you first assign a value to it.
 Variable Types:
         n..,-2,-1,0,-1,2,..n    is a Integer
         a-z or A-Z              is a String
@@ -109,6 +112,18 @@ x = 5                 # x is a Variable name        5 is a Variable
 y = "Hello, World!"   # y is a Variable name        "Hello, World!" is a Variable
 print (x)
 print (y)
+
+Variables do not need to be declared with any particular type, and can even change type after they have been set.
+x = 4       # x is of type integer
+x = "Sally" # x is now of type string
+print(x)
+
+Casting
+If you want to specify the data type of a variable, this can be done with casting.
+Example
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
 
 What is Python Comments?
         Python has commenting capability for the purpose of in-code documentation.
