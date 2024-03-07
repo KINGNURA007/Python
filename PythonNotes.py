3fca35d69231937a2dfde8651edcc805d6a6c761
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 14:49:00 +0530

    Update PythonNotes.py

--- PythonNotes.py
+++ PythonNotes.py
@@ -204,6 +204,75 @@ Each word is separated by an underscore character:
 Example
 my_variable_name = "John"
 
+What is Python Variables - Assign Multiple Values?
+        Many Values to Multiple Variables
+Python allows you to assign values to multiple variables in one line:
+Example
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+Note: Make sure the number of variables matches the number of values, or else you will get an error.
+
+One Value to Multiple Variables
+And you can assign the same value to multiple variables in one line:
+Example
+x = y = z = "Orange"
+print(x)
+print(y)
+print(z)
+
+Unpack a Collection
+        If you have a collection of values in a list, tuple etc. Python allows you to extract the values into variables. This is called unpacking.
+Example
+Unpack a list:
+fruits = ["apple", "banana", "cherry"]
+x, y, z = fruits
+print(x)
+print(y)
+print(z)
+
+What is Python - Output Variables?
+        The Python print() function is often used to output variables.
+Example
+x = "Python is awesome"
+print(x)
+
+In the print() function, you output multiple variables, separated by a comma:
+Example
+x = "Python"
+y = "is"
+z = "awesome"
+print(x, y, z)
+
+You can also use the + operator to output multiple variables:
+Example
+x = "Python "
+y = "is "
+z = "awesome"
+print(x + y + z)
+
+Notice the space character after "Python " and "is ", without them the result would be "Pythonisawesome".
+
+For numbers, the + character works as a mathematical operator:
+Example
+x = 5
+y = 10
+print(x + y)
+
+In the print() function, when you try to combine a string and a number with the + operator, Python will give you an error:
+Example
+x = 5
+y = "John"
+print(x + y)
+
+The best way to output multiple variables in the print() function is to separate them with commas, which even support different data types:
+Example
+x = 5
+y = "John"
+print(x, y)
+
 Python Comments
 ---------------
 
