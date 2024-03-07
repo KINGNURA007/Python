5c08107484c7c8a93b049a46ca3fd4ad28fd9b33
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 12:50:51 +0530

    Update Comments.py

--- Comments.py
+++ Comments.py
@@ -1,2 +1,31 @@
-#This is a 'Single-Line Comment'
-print('hello world')
+"""
+What is Python Comments?
+        Python has commenting capability for the purpose of in-code documentation.
+Comments can be used to explain Python code.
+Comments can be used to make the code more readable.
+Comments can be used to prevent execution when testing code.
+Comments Types:
+        Singleline Comments
+        Multiline Comments
+Singleline Comments
+Comments start with a #, and Python will render the rest of the line as a comment:
+Example
+Comments in Python:
+#This is a comment.
+print("Hello, World!")
+
+Comments can be placed at the end of a line, and Python will ignore the rest of the line:
+Example
+print("Hello, World!") #This is a comment.
+
+A comment does not have to be text that explains the code, it can also be used to prevent Python from executing code:
+Example
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+Multiline Comments
+        Python does not really have a syntax for multiline comments.
+Python will ignore string literals that are not assigned to a variable, you can add a multiline string (triple quotes) in your code, and place your comment inside it:
+Example
+"""print ("This is a comment written in more than just one line")"""
+print("Hello, World!")"""
