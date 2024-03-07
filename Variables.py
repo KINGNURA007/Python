2009dcded2bf7ead5769f4a5f58e6139178f93da
Mr.King007 <144191258+KINGNURA007@users.noreply.github.com>
2024-03-07 12:24:05 +0530

    Update Variables.py

--- Variables.py
+++ Variables.py
@@ -1,21 +1,15 @@
 """
 What is Python Variables?
         Variables is a reserved memory location to store values.
+Variable Types:
+        n..,-2,-1,0,-1,2,..n    is a Integer
+        a-z or A-Z              is a String
+        Container name = Variable Name
+        Storage container = Variable
+        Underscore Character is a Variable
 Example
 Variables in Python:"""
 x = 5                 # x is a Variable name        5 is a Variable
 y = "Hello, World!"   # y is a Variable name        "Hello, World!" is a Variable
 print (x)
 print (y)
-
-# Variable Types
-
-# n..,-2,-1,0,-1,2,..n    is a Integer
-
-# a-z or A-Z    is a String
-
-# Storage container = Variable"""
-
-# Container name = Variable Name
-
-# Underscore Character is a Variable
